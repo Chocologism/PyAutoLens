7f36d438132a21f6ab75b4552754c4c42fe2005b
dinghengkai_superpmo <dinghengkai22@mails.ucas.ac.cn>
2025-01-02 09:48:40 +0800

    add the non-parametric source plane Chi-squared: FitPositionsSourceFast

--- autolens/point/fit/positions/source/separations.py
+++ autolens/point/fit/positions/source/separations.py
@@ -7,6 +7,8 @@ from autolens.lens.tracer import Tracer
 from autolens.point.fit.positions.abstract import AbstractFitPositions
 from autolens.point.solver import PointSolver
 
+from typing import Optional, Tuple
+import numpy as np
 
 class FitPositionsSource(AbstractFitPositions):
     def __init__(
@@ -61,3 +63,88 @@ class FitPositionsSource(AbstractFitPositions):
         return self.model_data.distances_to_coordinate_from(
             coordinate=self.source_plane_coordinate
         )
+
+class FitPositionsSourceFast(AbstractFitPositions):
+    def __init__(
+        self,
+        name: str,
+        data: aa.Grid2DIrregular,
+        noise_map: aa.ArrayIrregular,
+        tracer: Tracer,
+        solver: Optional[PointSolver],
+        profile: Optional[ag.ps.Point] = None,
+        scale : float = 0.04,
+    ):
+        """
+        https://ui.adsabs.harvard.edu/abs/2020A%26A...636A..87C/abstract
+        https://ui.adsabs.harvard.edu/link_gateway/2010PASJ...62.1017O/EPRINT_PDF
+
+        Parameters
+        ----------
+        data : Grid2DIrregular
+            The (y,x) arc-second coordinates of positions which the maximum distance and log_likelihood is computed using.
+        noise_value
+            The noise-value assumed when computing the log likelihood.
+        scale
+            buffer used in hessian magnification, correspondding to pixel scale of the dataset
+        """
+
+        super().__init__(
+            name=name,
+            data=data,
+            noise_map=noise_map,
+            tracer=tracer,
+            solver=solver,
+            profile=profile,
+        )
+        self.scale = scale
+
+    @property
+    def model_data(self) -> aa.Grid2DIrregular:
+        """
+        Returns the model positions, which are computed via the point solver.
+
+        It if common for many more image-plane positions to be computed than actual positions in the dataset. In this
+        case, each data point is paired with its closest model position.
+        """
+        if len(self.tracer.planes) <= 2:
+            deflections = self.tracer.deflections_yx_2d_from(grid=self.data)
+        else:
+            deflections = self.tracer.deflections_between_planes_from(
+                grid=self.data, plane_i=0, plane_j=self.source_plane_index
+            )
+
+        return self.data.grid_2d_via_deflection_grid_from(deflection_grid=deflections)
+    
+    @property
+    def source_plane_coordinate(self) -> Tuple[float, float]:
+        """
+        https://ui.adsabs.harvard.edu/link_gateway/2010PASJ...62.1017O/EPRINT_PDF
+        """
+        model_data = self.model_data.array
+        sqrt_mag_map = self.sqrt_magnification_map
+        weight_sum = np.sum(sqrt_mag_map)
+        centre_0 = np.sum(sqrt_mag_map * model_data[:,0]) / weight_sum
+        centre_1 = np.sum(sqrt_mag_map * model_data[:,1]) / weight_sum
+
+        return (centre_0,centre_1)
+    
+    @property
+    def residual_map(self) -> aa.ArrayIrregular:
+        return self.model_data.distances_to_coordinate_from(
+            coordinate=self.source_plane_coordinate
+        )
+    
+    @property
+    def magnification_map(self) -> aa.Array2D:
+        return self.tracer.magnification_2d_via_hessian_from(self.data, buffer=self.scale)
+    @property
+    def abs_magnification_map(self) -> np.ndarray:
+        return np.abs(self.magnification_map.array)
+    @property
+    def sqrt_magnification_map(self) -> np.ndarray:
+        return np.sqrt(self.abs_magnification_map)
+
+    @property
+    def noise_map(self):
+        return self._noise_map / self.sqrt_magnification_map
